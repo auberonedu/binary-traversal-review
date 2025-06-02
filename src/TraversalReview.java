2d09266746204d17dbfaf735cb8aeaa2a8df1692
sandeersson <sandra.anderstrom@gmail.com>
2025-06-02 11:49:15 -0700

    Finished tripleSum

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -23,7 +23,9 @@ public class TraversalReview {
      * @return three times the sum of the nodes in the tree
      */
     public static int tripleSum(TreeNode node) {
-        return -1;
+        if (node == null) return 0;
+
+        return (3 * node.data) + tripleSum(node.left) + tripleSum(node.right);
     }
 
     /**
