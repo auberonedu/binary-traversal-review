74f2a12efc4ecd856be3ee9dcc8180468ebd4c2b
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-06-02 23:06:58 -0700

    Finished the postiveSum method

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -58,7 +58,14 @@ public class TraversalReview {
      * @return the sum of the positive values in the tree
      */
     public static int positiveSum(TreeNode node) {
-        return -1;
+        if (node == null) return 0;
+        int postiveSum = 0;
+        if (node.data > 0) {
+            postiveSum += node.data;
+        }
+        postiveSum += positiveSum(node.left);
+        postiveSum += positiveSum(node.right);
+        return postiveSum;
     }
 
     /**
