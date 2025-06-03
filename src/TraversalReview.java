a72c80a08297fcf977db6524b397db926fcd5ca2
konradkelly <konradky@gmail.com>
2025-06-03 09:51:03 -0700

    added positiveSum method

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -52,7 +52,12 @@ public class TraversalReview {
      * @return the sum of the positive values in the tree
      */
     public static int positiveSum(TreeNode node) {
-        return -1;
+        if (node == null) return 0;
+        int sum = 0;
+        if (node.data > 0) {
+            sum = node.data;
+        }
+        return sum + positiveSum(node.left) + positiveSum(node.right);
     }
 
     /**
