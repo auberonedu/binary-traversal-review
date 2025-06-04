20847113c44b038e63004cc626637f4179c2cf3b
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-06-03 22:24:39 -0700

    Finished concatenate method

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -278,6 +278,11 @@ public class TraversalReview {
      * @return a string with all the values of the tree concatenated in-order
      */
     public static String concatenate(TreeNode node) {
-        return "";
+        if (node == null) return "";
+        String numberConcat = "";
+        numberConcat += concatenate(node.left);
+        numberConcat += node.data;
+        numberConcat += concatenate(node.right);
+        return numberConcat;
     }
 }
