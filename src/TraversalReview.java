88de58e28babe73c96c44d8bcbe091dc2ea3d6c5
Jessica H <jessicahebert75@gmail.com>
2025-06-15 19:26:07 -0700

    Created a method to traverse the values in-order and returns it in a string

--- src/TraversalReview.java
+++ src/TraversalReview.java
@@ -264,6 +264,15 @@ public class TraversalReview {
      * @return a string with all the values of the tree concatenated in-order
      */
     public static String concatenate(TreeNode node) {
-        return "";
+
+        if (node == null) return "";
+
+        String results = "";
+
+        results+=concatenate(node.left);
+        results+= node.data;
+        results+=concatenate(node.right);
+
+        return results;
     }
 }
